dd24d3b22adc7a12bee0fe1159aa5408bd0293d3
cazaquedigital <rinattag999@gmail.com>
2026-05-25 10:06:14 -0300

    ch3

--- src/model/Degrau.java
+++ src/model/Degrau.java
@@ -1,25 +1,41 @@
 package model;
 
-public class Degrau extends Obstaculo {
-    private double alturaDegrau;
 
+public class Degrau extends Obstaculo {
+
+    private double alturaDegrau; 
     public Degrau(String localizacao, String descricao, int nivelDePerigo, double alturaDegrau) {
-        super(localizacao, descricao, nivelDePerigo);
+        super(localizacao, descricao, nivelDePerigo); 
         this.alturaDegrau = alturaDegrau;
     }
+
+   
+    public Degrau(String localizacao, String descricao, int nivelDePerigo) {
+        super(localizacao, descricao, nivelDePerigo);
+        this.alturaDegrau = 5.0; 
+    }
+
+
 
     public double getAlturaDegrau() { return alturaDegrau; }
     public void setAlturaDegrau(double alturaDegrau) { this.alturaDegrau = alturaDegrau; }
 
+   
     @Override
     public void exibirDetalhes() {
-        System.out.println("=== ALERTA DE DEGRAU ===");
+        System.out.println("=== ALERTA: DEGRAU ===");
         System.out.println(super.toString());
         System.out.println("Altura do degrau: " + alturaDegrau + " cm");
+        System.out.println("----------------------");
     }
 
+   
     @Override
     public void validarPosicao() {
-        System.out.println("Validando coordenadas do degrau em: " + getLocalizacao());
+        if (getLocalizacao() == null || getLocalizacao().isEmpty()) {
+            System.out.println("Erro: Localizacao do degrau nao informada.");
+        } else {
+            System.out.println("Posicao validada: degrau em " + getLocalizacao());
+        }
     }
 }
