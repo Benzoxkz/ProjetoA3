60fbadc8dc3a191cd80825ca85db7875ae92a90f
cazaquedigital <rinattag999@gmail.com>
2026-05-25 10:07:11 -0300

    ch3

--- src/model/RampaInexistente.java
+++ src/model/RampaInexistente.java
@@ -1,25 +1,43 @@
 package model;
 
-public class RampaInexistente extends Obstaculo {
-    private String localizacaoFaltante;
 
+public class RampaInexistente extends Obstaculo {
+
+    private String localizacaoFaltante; 
+
+   
     public RampaInexistente(String localizacao, String descricao, int nivelDePerigo, String localizacaoFaltante) {
         super(localizacao, descricao, nivelDePerigo);
         this.localizacaoFaltante = localizacaoFaltante;
     }
 
+   
+    public RampaInexistente(String localizacao, String descricao, int nivelDePerigo) {
+        super(localizacao, descricao, nivelDePerigo);
+        this.localizacaoFaltante = "Ponto nao especificado";
+    }
+
+    
+
     public String getLocalizacaoFaltante() { return localizacaoFaltante; }
     public void setLocalizacaoFaltante(String localizacaoFaltante) { this.localizacaoFaltante = localizacaoFaltante; }
 
+   
     @Override
     public void exibirDetalhes() {
-        System.out.println("=== ALERTA DE FALTA DE RAMPA ===");
+        System.out.println("=== ALERTA: RAMPA INEXISTENTE ===");
         System.out.println(super.toString());
-        System.out.println("Ponto exato onde deveria ter rampa: " + localizacaoFaltante);
+        System.out.println("Onde deveria ter rampa: " + localizacaoFaltante);
+        System.out.println("----------------------");
     }
 
+    
     @Override
     public void validarPosicao() {
-        System.out.println("Validando viabilidade de instalação de rampa em: " + getLocalizacao());
+        if (getLocalizacao() == null || getLocalizacao().isEmpty()) {
+            System.out.println("Erro: Localizacao nao informada.");
+        } else {
+            System.out.println("Posicao validada: rampa inexistente em " + getLocalizacao());
+        }
     }
 }
