133cda6cb1572815775ca427d149a238bb891700
cazaquedigital <rinattag999@gmail.com>
2026-05-25 10:06:45 -0300

    ch3

--- src/model/Obstaculo.java
+++ src/model/Obstaculo.java
@@ -1,15 +1,26 @@
 package model;
 
+
 public abstract class Obstaculo {
+
+
     private String localizacao;
     private String descricao;
-    private int nivelDePerigo;
+    private int nivelDePerigo; 
 
+ 
     public Obstaculo(String localizacao, String descricao, int nivelDePerigo) {
         this.localizacao = localizacao;
         this.descricao = descricao;
-        this.nivelDePerigo = nivelDePerigo;
+      
+        if (nivelDePerigo < 1 || nivelDePerigo > 10) {
+            System.out.println("Aviso: nivel de perigo deve ser entre 1 e 10. Valor ajustado para 1.");
+            this.nivelDePerigo = 1;
+        } else {
+            this.nivelDePerigo = nivelDePerigo;
+        }
     }
+
 
     public String getLocalizacao() { return localizacao; }
     public void setLocalizacao(String localizacao) { this.localizacao = localizacao; }
@@ -18,14 +29,25 @@ public abstract class Obstaculo {
     public void setDescricao(String descricao) { this.descricao = descricao; }
 
     public int getNivelDePerigo() { return nivelDePerigo; }
-    public void setNivelDePerigo(int nivelDePerigo) { this.nivelDePerigo = nivelDePerigo; }
+    public void setNivelDePerigo(int nivelDePerigo) {
+        if (nivelDePerigo < 1 || nivelDePerigo > 10) {
+            System.out.println("Nivel de perigo invalido. Mantendo valor anterior.");
+        } else {
+            this.nivelDePerigo = nivelDePerigo;
+        }
+    }
 
-    // Métodos a serem sobrescritos (Polimorfismo)
+  
     public abstract void exibirDetalhes();
+
+    
     public abstract void validarPosicao();
 
+    
     @Override
     public String toString() {
-        return "Local: " + localizacao + " | Descricao: " + descricao + " | Perigo (1-10): " + nivelDePerigo;
+        return "Local: " + localizacao +
+               " | Descricao: " + descricao +
+               " | Perigo (1-10): " + nivelDePerigo;
     }
 }
